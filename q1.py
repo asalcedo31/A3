9e9973b86516d414178502abeccb7e1f1248c564
asalcedo31 <adrisal31@gmail.com>
2017-12-06 01:02:55 -0500

    q1 cleaned

--- q1.py
+++ q1.py
@@ -11,6 +11,8 @@ from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
@@ -56,7 +58,7 @@ def tf_idf_features(train_data, test_data):
 def tf_cutoff(bow_train, tf_train, train_labels, cutoff=0):
     # training the baseline model
     mean_tf_idf = np.where(tf_train.mean(axis=0)>cutoff)[0]
-   # cut_tf = tf_train[:,mean_tf_idf]
+   
     print("mean idf", tf_train.shape)
     bow_train = bow_train[:,mean_tf_idf]
   
@@ -69,11 +71,15 @@ def tf_cutoff(bow_train, tf_train, train_labels, cutoff=0):
     train_pred = model.predict(binary_train)
     print('BernoulliNB baseline train accuracy = {}'.format((train_pred == train_labels).mean()))
     return (train_pred == train_labels).mean()
-def feature_selection(train,test,thresh=0.05):
-    sel = VarianceThreshold(threshold=0.00001)
+
+#feautre selection based on variance
+def feature_selection(train,test,thresh=0.00001):
+    sel = VarianceThreshold(threshold=thresh)
     train= sel.fit_transform(train)
     test = sel.transform(test)
     return(train,test)
+
+#mean tf idf cutoff for feature selection through cross validation --- didn't work
 def cutoff_cv(bow_train,train_tf,train_labels, cutoff_min, cutoff_max,step,folds=5):
     split_cv = KFold(n_splits=folds)
     cutoffs = np.arange(cutoff_min,cutoff_max,step=step)
@@ -106,68 +112,66 @@ def bnb_baseline(bow_train, train_labels, bow_test, test_labels):
 
     return test_pred
 
-def mnb_baseline(bow_train, train_labels, bow_test, test_labels):
-    # training the baseline model
-    binary_train = (bow_train>0).astype(int)
-    binary_test = (bow_test>0).astype(int)
-
+def mnb(bow_train, train_labels):
+    # multinomial naive bayes
     model = MultinomialNB()
     model.fit(bow_train, train_labels)
 
-    #evaluate the baseline model
+
     train_pred = model.predict(bow_train)
     print('MultinomialNB baseline train accuracy = {}'.format((train_pred == train_labels).mean()))
-    test_pred = model.predict(bow_test)
-    print(test_pred.min(),test_pred.max())
-    print('MultinomialNB baseline test accuracy = {}'.format((test_pred == test_labels).mean()))
-    return test_pred
+    return model
 
-def bagging(train,train_labels,test,test_labels):
+def gnb(train, train_labels):
+    # training the baseline model
+    model = GaussianNB()
+    model.fit(train.toarray(), train_labels)
+
+    #evaluate the baseline model
+    train_pred = model.predict(bow_train)
+    print('GaussianNB train accuracy = {}'.format((train_pred == train_labels).mean()))
+    return model
+
+def bagging(train,train_labels):
     model = MultinomialNB()
     
-    bag = BaggingClassifier(model,n_estimators=20,max_features=0.8)
-    model_grid = GridSearchCV(bag,param_grid=dict(n_estimators=np.arange(10,25,step=1),max_features=np.arange(0.1,1,step=0.05)))
+    bag = BaggingClassifier(model,n_estimators=25,max_features=0.8)
+    model_grid = GridSearchCV(bag,param_grid=dict(max_features=np.arange(0.1,1,step=0.05)))
     model_grid.fit(train,train_labels)
     bag.fit(train,train_labels)
     print(model_grid.best_params_)
     pred = model_grid.predict(train)
-    print((pred == train_labels).mean())
-    pred_test = model_grid.predict(test)
-    print((pred_test == test_labels).mean())
-    pass
-
+    print("MNB + Bagging train accuracy", (pred == train_labels).mean())
+    return(model_grid)
+#random forest
 def rfc(tf_train,tf_labels,tf_test, test_labels,C=1):
     model = sklearn.ensemble.RandomForestClassifier()
-    model_grid = GridSearchCV(model,param_grid=dict(n_estimators=np.arange(5,25,step=1),max_features=np.arrange(0,2*sqrt(tf_train.shape[1])/tf_train.shape[1],0.05),min_samples_leaf=np.arrange(1,50,step=1)))
+    model_grid = GridSearchCV(model,param_grid=dict(n_estimators=np.arange(7,21,step=1),max_features=np.arrange(0,sqrt(tf_train.shape[1])/tf_train.shape[1],0.05),min_samples_leaf=np.arrange(1,50,step=5)))
     model_grid.fit(tf_train,tf_labels)
     print(model_grid.best_params_)
     train_pred = model_grid.predict(tf_train)
     accuracy = (train_pred == tf_labels).mean()
     print(accuracy)
-    test_pred = model_grid.predict(tf_test)
-    accuracy = (test_pred == test_labels).mean()
-    print(accuracy)
-    return(test_pred)
+    return(model_grid)
 
-def svm_model(tf_train,tf_labels,tf_test, test_labels,C=1):
-    model = SVC(decision_function_shape='ovo')
+#svm
+def svm_model(tf_train,tf_labels):
+    model_grid = SVC(decision_function_shape='ovo')
+    
     model_grid = GridSearchCV(model,param_grid=dict(C=np.arange(0.01,1,step=0.1),kernel = ['linear','rbf','poly','sigmoid']))
     model_grid.fit(tf_train,tf_labels)
     print(model_grid.best_params_)
     train_pred = model_grid.predict(tf_train)
     accuracy = (train_pred == tf_labels).mean()
-    print(accuracy)
-    test_pred = model_grid.predict(tf_test)
-    accuracy = (test_pred == test_labels).mean()
-    print(accuracy)
-    return(test_pred)
+    print("SVM training accuracy:", accuracy)
+    return(model_grid)
 
 
 
-def log_reg2(tf_train,tf_labels,tf_test, test_labels,C=1):
+def log_reg2(tf_train,tf_labels):
     model = sklearn.linear_model.LogisticRegression(penalty='l2',
                                                    solver='sag',
-                                                   C=C,
+                                                   C=1,
                                                    fit_intercept = True,
                                                    random_state=42,
                                                    max_iter= 200,
@@ -178,11 +182,8 @@ def log_reg2(tf_train,tf_labels,tf_test, test_labels,C=1):
     print(model_grid.best_params_)
     train_pred = model_grid.predict(tf_train)
     accuracy = (train_pred == tf_labels).mean()
-    print(accuracy)
-    test_pred = model_grid.predict(tf_test)
-    accuracy = (test_pred == test_labels).mean()
-    print(accuracy)
-    pass
+    print("L2 logistic regression train accuracy: ", accuracy)
+    return(model_grid)
 
 
 def log_reg(tf_train,tf_labels, tf_test, test_labels,C=1,):
@@ -215,12 +216,15 @@ def log_reg(tf_train,tf_labels, tf_test, test_labels,C=1,):
     train_pred = model_grid.predict(tf_train)
     accuracy = (train_pred == tf_labels).mean()
     print(accuracy)
-    test_pred = model_grid.predict(tf_test)
-    accuracy = (test_pred == test_labels).mean()
-    print(accuracy)
-    pass
+    return(model)
 
-def confusion_matrix(pred,true, feature_names,filename):
+def run_test(model,data,truth,model_name):
+    pred = model.predict(data)
+    accuracy = (pred == truth).mean()
+    print(model_name, " test accuracy: ", accuracy)
+    return(pred)
+
+def confusion_matrix(pred,true, target_names,filename):
     cm = np.zeros((20,20))
     pred_truth = np.zeros(pred.shape[0],dtype={'names':('predicted','truth'),'formats':('int8','int8')})
     pred_truth['predicted'] = pred
@@ -228,33 +232,42 @@ def confusion_matrix(pred,true, feature_names,filename):
     for i in np.arange(0,20,step=1):
         for j in np.arange(0,20,step=1):
             cm[i,j] = pred_truth[(pred_truth['predicted'] == i ) & (pred_truth['truth']==j)].shape[0]
-    print(cm.shape)
-#    cm = np.insert(cm,int(0),feature_names,axis=0)
-#    cm = np.insert(cm,int(0),feature_names,axis=1)
-    df = pandas.DataFrame(data=cm, columns=feature_names)
-    np.savetxt(filename, cm, delimiter=",")
-    print(cm.sum())
+    df = pandas.DataFrame(data=cm,index=target_names)
+   
+    df.columns = target_names
+    df.to_csv(filename,float_format='% 6.0f')
+    print('total entries: ',cm.sum())
     return(cm)
-    
+
 if __name__ == '__main__':
     train_data, test_data = load_data()
     train_bow, test_bow, feature_names = bow_features(train_data, test_data)
-    print(train_data.target)
-    print(np.__version__)
+    
     train_tf, test_tf, feature_names = tf_idf_features(train_data,test_data)
-    train_tf,test_tf = feature_selection(train_tf,test_tf)
-    print(train_tf.shape)
-    print(test_tf.shape)
-    print((train_tf.mean(axis=0)).max(axis=1))
+    train_tf,test_tf = feature_selection(train_tf,test_tf,0.00001)
     #tf_train.min(axis=1)
    # log_reg(train_tf,train_data.target,test_tf, test_data.target)
   #  log_reg2(train_bow,train_data.target,test_bow, test_data.target)
    # cv_cutoff(train_bow,train_labels,train_tf,)
  #   bnb_cutoff_model = cutoff_cv(train_bow, train_tf, train_data.target,cutoff_min=0, cutoff_max = (train_tf.mean(axis=0)).max(axis=1), step = ((train_tf.mean(axis=0)).max(axis=1))/10 )
     bnb_model = bnb_baseline(train_bow, train_data.target, test_bow, test_data.target)
-    mnb_model = mnb_baseline(train_tf, train_data.target, test_tf, test_data.target)
-#    confusion_matrix(mnb_model,test_data.target,feature_names,'mnb_confusion.csv')
- #   bag_model = bagging(train_tf, train_data.target, test_tf, test_data.target)
-    svm_out = svm_model(train_tf, train_data.target, test_tf, test_data.target)
-  #  print(Counter(train_data.target))
- #   log_reg2(train_tf,train_data.target,test_tf, test_data.target)
+   
+    mnb_model = mnb(train_bow, train_data.target)
+    mnb_pred = run_test(mnb_model,test_bow,test_data.target,"MultinomialNB")
+    confusion_matrix(mnb_pred,test_data.target,test_data.target_names,'mnb_confusion.csv')
+    
+    bag_model = bagging(train_bow, train_data.target)
+    bag_pred = run_test(bag_model,test_bow,test_data.target,"Bagged MultinomialNB")
+    confusion_matrix(bag_pred,test_data.target,test_data.target_names,'bg_mnb_confusion.csv')
+    
+    log_reg_model = log_reg2(train_tf,train_data.target)
+    log_pred = run_test(log_reg_model,test_tf,test_data.target,"L2 penalized logistic regression")
+    confusion_matrix(log_pred,test_data.target,test_data.target_names,'log_confusion.csv')
+    
+    svm_out = svm_model(train_tf, train_data.target)
+    svm_pred = run_test(svm_out,test_tf,test_data.target,"SVM")
+    confusion_matrix(svm_pred,test_data.target,test_data.target_names,'svm_confusion.csv')                    
+ 
+    rfc_mod = rfc(train_tf,train_data.target,test_tf, test_data.target)
+    rfc_pred = run_test(rfc_mod,test_tf,test_data.target,"Random Forest")
+    confusion_matrix(rfc_pred,test_data.target,test_data.target_names,'rfc_confusion.csv') 
